5ed6093390d5489d3eef41b7c69ce4ef27247ce5
Otoniel Garcia <mardoqueogarcia112016@gmail.com>
2023-10-19 09:13:20 -0600

    schema_data, HTTPException, clean code

--- My-Movie-Project/data/__init__.py
+++
@@ -1,4 +0,0 @@
-import json #para usar el formato de lectura json
-file = open("data/datas.json") #abrimos el archivo
-data = json.load(file) #cargamos ese archivo en la variable data
-file.close #cerramos el archivo
--- My-Movie-Project/main.py
+++ My-Movie-Project/main.py
@@ -1,66 +1,57 @@
-from fastapi import FastAPI, Body #importamos el modulo
+from fastapi import FastAPI, Body, HTTPException #Importamos la clase body para requerimientos en el cuerpo de la peticion, HTTPException para mansar mensajes personalizados de codigo dependiendo del status_code.
 from fastapi.responses import HTMLResponse, FileResponse #para retornar codigo html, fileresponse para retornar el archivo html, htmlresponse para retornar codigo escrito dentro de la funcion. 
-import data #importamos para traer el archivo json
-#movies = data.data #almacenamos la informacion en la variable movies
-#Manera 1
-'''
-app = FastAPI() #creamos una instancia de fastapi almacenada en la variable app
-app.title = "Movies server --Beta" #nombre que se le da al titulo de nuestra documentacion en la web.
-app.version = "0.0.1" #Le estamos escribiendo la version actual que tiene nuestro servidor
-'''
-#Manera 2
+from pydantic import BaseModel, Field #BaseModel para crear esquemas de datos, Field es una clase para validacion de datos.
+from typing import Optional #Para que podamos poner como opcional un dato
+import movies as db #importamos la base de datos de las peliculas
+import datetime #modulo destinado a fechas
+
+#Esquema de datos generales de las peliculas
+class Movie(BaseModel):
+    id : Optional[int] = None #para que el id sea opcional
+    title : str = Field(min_length=5, max_length=20) #le estamos definiendo cual es el linite de caracteres de para el titulo
+    overview : str = Field(min_length=10, max_length=50)
+    year : int = Field(ge=1980, le=datetime.date.today().year) #ge ==> para que el año ingresado sea mayor a 1980, le ==> para definir que el año debe ser menor o igual a 2022.
+    rating : float = Field(ge=0, le=9.9)
+    category : str = Field(min_length=5, max_length=10)
+
+    #clase para definir los valores por defecto del body
+    class Config:
+        json_schema_extra = {
+            'example':
+            {
+                'id':len(db.Data) + 1,
+                'title':'My new movie',
+                'overview':'Descripcion breve',
+                'year': 2022,
+                'rating': 9.2,
+                'category': 'accion'
+            }
+        }
+    #codigo debido al cambio de version a pydantic v2
+    '''
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "id": len(db.Data) + 1,
+                    "title": "New movie",
+                    "overview": "Descripcion breve",
+                    "year": 2022,
+                    "rating":9.5,
+                    "category": "accion"
+                }
+            ]
+        }
+    }
+    '''
+
 app = FastAPI(
     title= "Movies Server --Beta",
-    description= "Aca encontraras un servidor de peliculas, por: -Categorias -Mejor puntuacion -Por año",
+    description= "Aca encontraras un servidor de peliculas Latinoamericanas usando una base de datos local.",
     version= "0.0.2"
 )
 
-
-#diccionario de la informacion de las peliculas
-movies = [
-    {
-        'id':1,
-        'title':'Step up 3',
-        'overview':'El baile callejero underground de Nueva York constituye el...',
-        'year': 2010,
-        'rating':8.9,
-        'category': "baile"
-    },
-    {
-        'id':2,
-        'title':'Asi en la tierra como en el infierno',
-        'overview':'La arqueóloga Scarlett Marlowe ha dedicado toda su vida a encontrar...',
-        'year': 2014,
-        'rating':8.1,
-        'category': "terror"
-    },
-    {
-        'id':3,
-        'title':'Triunfos robados',
-        'overview':'La estudiante de primer año de la universidad Whittier y su amiga Monica forman parte...',
-        'year': 2004,
-        'rating':8.1,
-        'category': "baile"
-    },
-    {
-        'id':4,
-        'title':'Camino hacia el terror',
-        'overview':'En un lugar de Chernobyl, era un pueblo olvidado debido a que...',
-        'year': 2002,
-        'rating':9.0,
-        'category': "terror"
-    },
-    {
-        'id':5,
-        'title':'El Unico',
-        'overview':'Jhon es un policia que trabaja sin preocupaciones de nada, sin embargo un dia de...',
-        'year': 2010,
-        'rating':8.1,
-        'category': "accion"
-    }
-]
-
-
+movies =db.Data #importamos las peliculas.
 
 #METODOS GET
 @app.get('/', tags=['Home']) #nuestra ruta inicial, la etiqueta (tags) sirve para  agrupar determinadas rutas de nuestra aplicación
@@ -68,70 +59,43 @@ def message():
     #Forma de retornar un html con codigo dentro de return
      return HTMLResponse(
          '''<h1>PelisMar</h1>
-           <h4>Servidor de diversas peliculas del cine Latinoamericano</h4>
+           <h4>Servidor de peliculas del cine Latinoamericano</h4>
            <p>Puedes ejecutar la documentación swagger colocando en la url de la web /docs. Eso te abrira la documentacion para que puedas ver todos los metodos de nuestro servidor.</p>
          ''')
-
     #return FileResponse('index.html') #De esta forma devolvemos un archivo html completo
 
 #metodo para retornar todas las peliculas
 @app.get('/movies', tags=['Movies'])
 def get_movies():
-    return movies #traemos el archivo de las peliculas
+    return movies
 
 #Mediante parametros
 @app.get('/movies/{movie_id}', tags=['Movies']) #dentro de las llaves le especificamos que parametro debe ingresar
 def get_movies_by_id(movie_id : int): #de esta forma le definimos el parametro y el tipo de dato que debe recibir
-    #codigo 1 (de la clase)
-    for item in movies: #iteramos la lista de las peliculas
-        if item['id']==movie_id: #validamos si el id ingresado esta en el id del listado
-            return item #si ese fuese el caso, nos retornara la informacion de la pelicula basada en ese id
-    return 'El id ingresado no existe' #de lo contrario nos arrojara un mensaje
+    response = [item for item in movies if item['id']==movie_id]
+    if len(response) > 0: return response
+    raise HTTPException(status_code=404, detail='Id not Found') #nos retornara codigo de error y su mensaje personalizado
     
-    '''
-    #codigo de prueba(mio)
-    try:
-        return movies[movie_id -1]
-    except :
-        countt = len(movies)
-        return f"Error, limite de id es {countt}"
-
-    #codigo 2 (aporte)
-    try:
-        return movies[movie_id - 1]
-    except IndexError:
-        return {"error": "Movie not found"}
-    
-    #codigo 3 (aporte)
-    movie = list(filter(lambda x: x['id'] == movie_id,movies))
-    return "Movie not found" if not movie else movie[0]
-    '''
 #Metodo mediante parametros query
-@app.get('/movies/', tags=['Movies']) #le agregamos el slash a la ruta para que detecte que se agregara un parametro query y no nos remplace el otro metodo creado anteriormente.
-def get_movies_by_category(category:str, year:int): #no ingresamos la variable dentro de la url ya que automaticamente fastapi lo detectara como un parametro query.
+@app.get('/movies/category/', tags=['Movies']) #le agregamos el slash a la ruta para que detecte que se agregara un parametro query y no nos remplace el otro metodo creado anteriormente.
+def get_movies_by_category(category:str): #no ingresamos la variable dentro de la url ya que automaticamente fastapi lo detectara como un parametro query.
+    response = [movie for movie in movies if movie['category']==category]
+    if len(response) > 0: return response
+    raise HTTPException(status_code=404, detail='Movie by category not Found')
 
-    #codigo 1 (mi aporte)
-    try: 
-        response = [movie for movie in movies if movie['category']==category and movie['year']==year]
-        if len(response) > 0: return response
-        else: return 'Movies no found by category and year'
-    except: 
-        return 'Error en el servidor remoto'
-
+#Para buscar mediante año de publicacion
+@app.get('/movies/publicate/{movie_year}', tags=['Movies'])
+def get_movie_by_year(movie_year : int):
+    response = [items for items in movies if items['year']==movie_year]
+    if len(response) > 0: return response
+    raise HTTPException(status_code=404, detail='Movie by year not Found')
+    
     '''
     #codigo 2 (aporte)
-    #mostrando lista mediante list comprenhesion
-    response = [movie for movie in movies if movie['category'] == category]
-    if len(response) > 0:
-        return response
-    else:
-        return "Movies not found"
-
-    #codigo 3 (aporte)
     #mostrando la lista mediante la funcion filter
     return list(filter(lambda item: item['category'] == category , movies))
 
-    #codigo 4 (aporte)
+    #codigo 3 (aporte)
     # Filtrar las películas por categoría y año
     filtered_movies = [movie for movie in movies if movie['category'] == category and movie['year'] == year]
     # Si hay películas que cumplen (o sea la lista tiene elementos)
@@ -140,83 +104,30 @@ def get_movies_by_category(category:str, year:int): #no ingresamos la variable d
     return f'No hay películas para la categoría {category} y el año {year}'
     '''
 
-#METODO POST
+#METODO POST (para registrar nuevas peliculas)
 @app.post('/movies', tags=['Movies'])
-#para registrar nuevas peliculas
-#Codigo de la clase
-#El body nos sirve para que los parametros no se tomen como un parametro query si no como peticion del body, el default es lo que nos mostrara por defecto en el body
-def create_movie(id:int = Body(default=len(movies)+1), title:str = Body(default="Text"), overview:str = Body(default="Text"), year:int = Body(default=2023), rating:float = Body(default=5.0), category:str = Body(default='Text')):
-    #agregamos a nuestra lista de las peliculas
-    movies.append({
-        "id":id,
-        "title":title,
-         "overview":overview,
-         "year":year,
-         "rating":rating,
-         "category":category
-     })
+def create_movie(movie: Movie): #requerimos una pelicula de la clase Movie
+    movies.append(movie) #de esta manera registramos directamente la pelicula.
     return movies
 
-'''
-#Codigo 2 (Aporte)
-from pydantic import BaseModel #para crear el modelo
-class Movie(BaseModel): #creamos una clase donde estaran las variables que usaremos como parametros
-    id:int
-    title:str
-    overview:str
-    year:int
-    rating:float
-    category:str
-@app.post('/movies', tags=['Movies'],response_model = list[Movie]) #agregamos una lista del modelo
-def create_movie(new_movie: Movie):
-    movies.append(new_movie)
-    return movies
-'''
-
-#METODO PUT
-#para actualizar una pelicula
+#METODO PUT (para actualizar una pelicula)
 @app.put('/movies/{id}', tags=['Movies']) #pediremos el id como parametro
-def update_movie(id:int, title:str = Body(default="Text"), overview:str = Body(default="Text"), year:int = Body(default=2023), rating:float = Body(default=5.0), category:str = Body(default='Text')):
+def update_movie(id:int, movie : Movie):
     for items in movies:
         if items["id"]==id:
-            items["title"]=title
-            items["overview"]=overview
-            items["year"]=year
-            items["rating"]=rating
-            items["category"]=category
+            items["title"]=movie.title
+            items["overview"]=movie.overview
+            items["year"]=movie.year
+            items["rating"]=movie.rating
+            items["category"]=movie.category
             return movies
-    return "Movie by id not found"
+    raise HTTPException(status_code=404, detail='Movie by id not found in database')
 
-'''
-#mi codigo de aporte al reto
-from pydantic import BaseModel
-class Items(BaseModel):
-    title:str
-    overview:str
-    year:int
-    rating:int
-    category:str
-@app.put('/movies/{id_movie}',tags=['Movies'])
-def update_movie(id_movie:int, movie:Items):
-    return {'id':id_movie, 'title':movie.title, 'overview':movie.overview, 'year':movie.year, 'rating':movie.rating, 'category':movie.category}
-'''
-
-#METODO DELETE
-#para eliminar una pelicula mediante el id que nos manden
+#METODO DELETE (para eliminar una pelicula mediante el id que nos manden)
 @app.delete('/movies/{id}', tags=['Movies'])
 def delete_movie(id:int):
     for items in movies:
         if items["id"]==id:
             movies.remove(items)
             return movies
-
-'''
-#Codigo de aporte al reto
-@app.delete('/movies/{id_movie}', tags=['Movies'])
-def delete_movie(id_movie: int):
-    for item in movie:
-        if item["id"]==id:
-            movies.remove(item)
-            return movies
-    return "Error, id not found"
-'''
+    raise HTTPException(status_code=404, detail='Remove by id not found')
